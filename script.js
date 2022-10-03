3b3c9a97f4d3dc3e983f231af3c2de43726d9472
claudeando <claude.mm6@gmail.com>
2022-10-03 15:11:46 +0100

    test

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ c.height = window.innerHeight
 
 const webgl = c.getContext('webgl') || c.getContext('experimental-webgl')
 
-webgl = clearColor(0.0, 0.00, 0.0, 1.0)
+webgl = clearColor(255, 0.0, 0.0, 1.0)
 webgl = clearDepth(1.0)
 webgl = clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
