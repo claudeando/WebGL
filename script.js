27c6ef2b21b12a3f113f70b12fb4786c12a9f7bc
claudeando <claude.mm6@gmail.com>
2022-10-04 19:29:03 +0100

    glsl

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+// Canvas要素の取得
+const canvas = document.querySelector('canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
+// WebGl Contextの取得　（これは何なのか）
+const context = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
 
-const c = document.getElementById('WebGl')
-c.width = window.innerWidth
-c.height = window.innerHeight
-
-const webgl = c.getContext('webgl') || c.getContext('experimental-webgl')
-
+// WebGl Canvasの初期化
 webgl = clearColor(255, 0.0, 0.0, 1.0)
 webgl = clearDepth(1.0)
 webgl = clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+// Shader: 書く順番は決まっていて、Vertex Shader -> Fragment Shader になります。
+// Shader (GLSL) should be written in HTML
